c8c26ff0106ff6c74b11e14f4f70fcbf03c0c07e
katacek <katacek@gmail.com>
2020-03-23 22:07:10 +0100

    update

--- main.js
+++ main.js
@@ -7,31 +7,35 @@ let check = false;
 Apify.main(async () =>
 {
 
-    // const kvStore = await Apify.openKeyValueStore('COVID-19-KOSOVO');
-    // const dataset = await Apify.openDataset('COVID-19-KOSOVO-HISTORY');
-    // const { email } = await Apify.getValue('INPUT');
+    const kvStore = await Apify.openKeyValueStore('COVID-19-KOSOVO');
+    const dataset = await Apify.openDataset('COVID-19-KOSOVO-HISTORY');
+    const { email } = await Apify.getValue('INPUT');
 
     console.log('Launching Puppeteer...');
     const browser = await Apify.launchPuppeteer();
 
     const page = await browser.newPage();
-    await Apify.utils.puppeteer.injectJQuery(page);
-
+    
     console.log('Going to the website...');
     await page.goto('https://kosova.health/en/');
+
+    await Apify.utils.puppeteer.injectJQuery(page);
       
-    await page.waitForSelector('#__layout > div > div:nth-child(2) > section:nth-child(3) > div > div:nth-child(2) > div:nth-child(1) > div');
+    await page.waitForSelector('div.statistics-box.box-shadow');
     
     console.log('Getting data...');
     // page.evaluate(pageFunction[, ...args]), pageFunction <function|string> Function to be evaluated in the page context, returns: <Promise<Serializable>> Promise which resolves to the return value of pageFunction
     const result = await page.evaluate(() =>
     {
      
-        // eq() selector selects an element with a specific index number, text() method sets or returns the text content of the selected elements
-        const identifiedCases = $("#__layout > div > div:nth-child(2) > section:nth-child(3) > div > div:nth-child(2) > div:nth-child(1) > div > h3");
-        const recovered = $("#__layout > div > div:nth-child(2) > section:nth-child(3) > div > div:nth-child(2) > div:nth-child(2) > div > h3");
-        const deceased = $("#__layout > div > div:nth-child(2) > section:nth-child(3) > div > div:nth-child(2) > div:nth-child(3) > div > h3").text();
-        
+        const now = new Date();
+        // text() method sets or returns the text content of the selected elements
+                
+        const identifiedCases = $('span:contains(Identified Cases)').prev().text()
+        const recovered = $('span:contains(Recovered)').prev().text()
+        const deceased = $('span:contains(Deceased)').prev().text()
+
+
         const data = {
             identifiedCases: identifiedCases,
             recovered: recovered,
@@ -51,40 +55,40 @@ Apify.main(async () =>
             }
         
     
-    // let latest = await kvStore.getValue(LATEST);
-    // if (!latest) {
-    //     await kvStore.setValue('LATEST', result);
-    //     latest = result;
-    // }
-    // delete latest.lastUpdatedAtApify;
-    // const actual = Object.assign({}, result);
-    // delete actual.lastUpdatedAtApify;
+    let latest = await kvStore.getValue(LATEST);
+    if (!latest) {
+        await kvStore.setValue('LATEST', result);
+        latest = result;
+    }
+    delete latest.lastUpdatedAtApify;
+    const actual = Object.assign({}, result);
+    delete actual.lastUpdatedAtApify;
 
-    // if (JSON.stringify(latest) !== JSON.stringify(actual)) {
-    //     await dataset.pushData(result);
-    // }
+    if (JSON.stringify(latest) !== JSON.stringify(actual)) {
+        await dataset.pushData(result);
+    }
 
-    // await kvStore.setValue('LATEST', result);
-    // await Apify.pushData(result);
+    await kvStore.setValue('LATEST', result);
+    await Apify.pushData(result);
 
 
-    // console.log('Closing Puppeteer...');
-    // await browser.close();
-    // console.log('Done.');  
+    console.log('Closing Puppeteer...');
+    await browser.close();
+    console.log('Done.');  
     
-    // if there are no data for TotalInfected, send email, because that means something is wrong
-    // const env = await Apify.getEnv();
-    // if (check) {
-    //     await Apify.call(
-    //         'apify/send-mail',
-    //         {
-    //             to: email,
-    //             subject: `Covid-19 UK from ${env.startedAt} failed `,
-    //             html: `Hi, ${'<br/>'}
-    //                     <a href="https://my.apify.com/actors/${env.actorId}#/runs/${env.actorRunId}">this</a> 
-    //                     run had 0 TotalInfected, check it out.`,
-    //         },
-    //         { waitSecs: 0 },
-    //     );
-    // };
+    //if there are no data for TotalInfected, send email, because that means something is wrong
+    const env = await Apify.getEnv();
+    if (check) {
+        await Apify.call(
+            'apify/send-mail',
+            {
+                to: email,
+                subject: `Covid-19 UK from ${env.startedAt} failed `,
+                html: `Hi, ${'<br/>'}
+                        <a href="https://my.apify.com/actors/${env.actorId}#/runs/${env.actorRunId}">this</a> 
+                        run had 0 identifiedCases, check it out.`,
+            },
+            { waitSecs: 0 },
+        );
+    };
 });
